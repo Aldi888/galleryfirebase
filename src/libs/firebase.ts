d6d2e7af6dc1f7d8d7a7cea181ab55f5dc053ffd
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-05 18:49:57 -0300

    fire

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: Process.env.REACT_APP_FIREBASE_APIKEY
-  authDomain: Process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: Process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: Process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: Process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: Process.env.REACT_APP_FIREBASE_MENSSAGINGSENDERID,
-  appId: Process.env.REACT_APP_FIREBASE_APPID,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MENSSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
   measurementId: Process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
